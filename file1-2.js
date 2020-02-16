010237ec7f72163ef7adc55a6923513a5d786fc4
tnphamnguyen <tn.phamnguyen@outlook.com>
2020-02-16 15:32:58 +0100

    func js revNom

---
+++ file1-2.js
@@ -0,0 +1,15 @@
+function nomRev(str) {
+    let strRev="";
+    for(i=(str.length-1);i>=0;i--){
+        strRev=strRev+str[i];
+    }
+    return strRev;
+}
+function verifNomRev(string){
+    let nomRev=nomRev(string);
+    if(nomRev===string){
+        return true;
+    }else{
+        return false;
+    }
+}
